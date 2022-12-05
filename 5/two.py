9ecd6ed31fe21629e3ae66b24b1318df90130380
teej <tj@teej.io>
2022-12-05 08:55:50 -0700

    day five done
    parsing logic is a bit jank, but got to use re.split() which made it easier :)

---
+++ 5/two.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+import re
+
+def get_input(filename):
+    f = open(filename, mode='r')
+    
+    rows = []
+    while f:
+        line = f.readline()
+        if line.__contains__("1"):
+            break
+
+        row_crates = list(map(lambda x: x.replace('[', '').replace(']', '').replace('\n', ''), re.split(r'    |] \[| \[|] ', line)))
+        rows.append(row_crates)
+    
+    stacks = []
+    for i in range(0, len(rows[0])):
+        stack = []
+        for j in range(len(rows)-1, -1, -1):
+            if rows[j][i]:
+                stack.append(rows[j][i])
+        stacks.append(stack)
+
+    instructions = []
+    lines = f.readlines()
+    
+    for line in lines:
+        line = line.strip()
+        if not line:
+            continue
+        ins = re.split(r'move | from | to ', line)
+        ins.remove('')
+        ins = list(map(lambda x: int(x), ins))
+        instructions.append(ins)
+    
+    return (stacks, instructions)
+    
+    
+
+def main():
+    stacks, instructions = get_input("./input1.txt")
+    print(stacks)
+
+    for ins in instructions:
+        qty, src, dest = ins
+
+        # ...gotta offset by one sigh
+        src -= 1
+        dest -= 1
+        
+        items = stacks[src][-qty:]
+        stacks[dest].extend(items)
+
+        for i in range(qty):
+            stacks[src].pop()
+
+
+
+    for stack in stacks:
+        print(stack[-1], end="")
+    
+if "__main__" in __name__:
+    main()
